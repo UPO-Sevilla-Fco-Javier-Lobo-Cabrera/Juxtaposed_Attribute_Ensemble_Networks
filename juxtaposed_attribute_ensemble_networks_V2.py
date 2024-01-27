79d5e8c098339fa98d16abaaa67644f5b8be5be9
UPO-Sevilla-Fco-Javier-Lobo-Cabrera <francisco.lobo6@gmail.com>
2024-01-27 14:30:05 +0100

    Update juxtaposed_attribute_ensemble_networks_V2.py

--- juxtaposed_attribute_ensemble_networks_V2.py
+++ juxtaposed_attribute_ensemble_networks_V2.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_predict
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import precision_score, recall_score, f1_score, roc_auc_score
